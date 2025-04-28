42063af88e138657c4121d80b7c6b4d777c5b4d8
Abhi-10949 <23bcs10949@cuchd.in>
2025-04-28 19:43:28 +0530

    Week5 and day 29

---
+++ week_5/Day_29/Valid_Anagram.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+
+
+
+public class Valid_Anagram {
+    public static boolean isAnagram(String s, String t){
+        if(s.length()  != t.length()) return false;
+
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        return Arrays.equals(sArr, tArr);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("rat", "car"));
+    }
+}
