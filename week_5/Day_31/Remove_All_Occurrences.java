65ec295a015fe82984ee1172a3388535c0013544
Abhi-10949 <23bcs10949@cuchd.in>
2025-04-30 21:58:32 +0530

    day 31

---
+++ week_5/Day_31/Remove_All_Occurrences.java
@@ -0,0 +1,18 @@
+public class Remove_All_Occurrences {
+    public static String removeOccurrences(String s, String part) {
+        while (s.contains(part)) {
+            s = s.replaceFirst(part, "");
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "daabcbaabcbc";
+        String part1 = "abc";
+        System.out.println(removeOccurrences(s1, part1)); // Output: dab
+
+        String s2 = "axxxxyyyyb";
+        String part2 = "xy";
+        System.out.println(removeOccurrences(s2, part2)); // Output: ab
+    }
+}
