79c4bd5fe1d2f83d0926dc978c1c57566fc5273c
Abhi-10949 <23bcs10949@cuchd.in>
2025-05-03 21:12:12 +0530

    day 34

---
+++ week_5/Day_34/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+
+
+public class LongestCommonPrefix {
+    public static String longestCommoString(String []strs){
+        if(strs == null ||strs.length == 0) return "";
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        int i =0;
+        while(i<first.length() && i<last.length() && first.charAt(i)==last.charAt(i)){
+            i++;
+        }
+        return first.substring(0, i);
+    }
+    public static void main(String []args){
+        LongestCommonPrefix obj = new LongestCommonPrefix();
+        String []strs = {"flower","flow","flight"};
+        String result = obj.longestCommoString(strs);
+        System.out.println("Longest common Prefix : "+ result);
+    }
+}
