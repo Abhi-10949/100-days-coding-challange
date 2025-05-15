371b410f3e7acbb1108528edbef62e4cbf86f8c0
Abhi-10949 <23bcs10949@cuchd.in>
2025-05-15 21:54:48 +0530

    Day 46

---
+++ week_7/Day_46/GetLongestSubsequence.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class GetLongestSubsequence {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> result = new ArrayList<>();
+        int last = -1;
+        for(int i = 0;i<words.length;i++){
+            if(groups[i] != last){
+                result.add(words[i]);
+                last = groups[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String []args){
+        GetLongestSubsequence obj=  new GetLongestSubsequence();
+        String []words = {"e","a","b"};
+        int[] groups = {0,0,1};
+        List <String> result = obj.getLongestSubsequence(words, groups);
+
+        for(String word : result){
+            System.out.print(word+" ");
+        }
+        System.out.println();
+    }
+}
