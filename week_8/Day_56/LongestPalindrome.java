b8eadcc78f6d6062e2a9c95b683ea2a3dfd73996
Abhi-10949 <23bcs10949@cuchd.in>
2025-05-25 22:46:30 +0530

    Day 56

---
+++ week_8/Day_56/LongestPalindrome.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class LongestPalindrome {
+    public int longestPalindrome(String[] words) {
+        Map<String, Integer> mpp = new HashMap<>();
+        for (String w : words)
+            mpp.put(w, mpp.getOrDefault(w, 0) + 1);
+
+        int count = 0, alreadyPalindrome = 0;
+        for (Map.Entry<String, Integer> e : mpp.entrySet()) {
+            String w = e.getKey();
+            int freq = e.getValue();
+            String s = new StringBuilder(w).reverse().toString();
+            if (w.equals(s)) {
+                count += (freq / 2) * 4;
+                if (freq % 2 == 1)
+                    alreadyPalindrome = 1;
+            } else if (w.compareTo(s) < 0 && mpp.containsKey(s)) {
+                count += Math.min(freq, mpp.get(s)) * 4;
+            }
+        }
+        return count + alreadyPalindrome * 2;
+    }
+
+    public static void main(String[] args) {
+        LongestPalindrome sol = new LongestPalindrome();
+        String[] words = {"lc", "cl", "gg"};
+        int result = sol.longestPalindrome(words);
+        System.out.println("Longest palindrome length: " + result);
+    }
+}
