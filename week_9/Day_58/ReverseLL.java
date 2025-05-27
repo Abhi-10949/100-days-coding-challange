3df6505ea0d93c4f1566219bb22b5ede6c50c56c
Abhi-10949 <23bcs10949@cuchd.in>
2025-05-27 20:14:57 +0530

    Day 58

---
+++ week_9/Day_58/ReverseLL.java
@@ -0,0 +1,56 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class ReverseLL {
+    public ListNode reverseList(ListNode head) {
+        ListNode node = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+        return node;
+    }
+
+    // Helper method to print linked list
+    public void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode n5 = new ListNode(5);
+        ListNode n4 = new ListNode(4, n5);
+        ListNode n3 = new ListNode(3, n4);
+        ListNode n2 = new ListNode(2, n3);
+        ListNode head = new ListNode(1, n2);
+
+        ReverseLL sol = new ReverseLL();
+        System.out.print("Original List: ");
+        sol.printList(head);
+
+        ListNode reversed = sol.reverseList(head);
+
+        System.out.print("Reversed List: ");
+        sol.printList(reversed);
+    }
+}
