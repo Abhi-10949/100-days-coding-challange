a516d025b12fb7e779ebeba5fe74c149283b453b
Abhi-10949 <23bcs10949@cuchd.in>
2025-06-02 20:25:05 +0530

    Day 64

---
+++ week_10/Day_64/RemoveNtFromEnd.java
@@ -0,0 +1,52 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class RemoveNtFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) return head.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    // Utility function to print the list
+    public void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Creating the linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int n = 2; // Remove the 2nd node from the end
+
+        RemoveNtFromEnd sol = new RemoveNtFromEnd();
+        System.out.print("Original list: ");
+        sol.printList(head);
+
+        head = sol.removeNthFromEnd(head, n);
+
+        System.out.print("Updated list: ");
+        sol.printList(head);
+    }
+}
