49b70e863a0ec477ea2fc0c1af9535450f3cc1e0
Abhi-10949 <23bcs10949@cuchd.in>
2025-06-10 20:30:13 +0530

    Day 72

---
+++ week_11/Day_72/Main.java
@@ -0,0 +1,32 @@
+class MaxDifference {
+    public int maxDifference(String s) {
+        int[] mpp = new int[26];
+        int maxi = 0, mini = s.length();
+
+        for (char c : s.toCharArray()) {
+            mpp[c - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (mpp[i] % 2 != 0) {
+                maxi = Math.max(maxi, mpp[i]);
+            }
+            if (mpp[i] % 2 == 0 && mpp[i] > 0) {
+                mini = Math.min(mini, mpp[i]);
+            }
+        }
+
+        return maxi - mini;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MaxDifference solution = new MaxDifference();
+
+        String s = "aabbccddeef";
+        int result = solution.maxDifference(s);
+
+        System.out.println("Max Difference: " + result);
+    }
+}
