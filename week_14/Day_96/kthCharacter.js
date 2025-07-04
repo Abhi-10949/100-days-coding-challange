a91a147fc40b2d312beb78be368b601a514a1b48
Abhi-10949 <23bcs10949@cuchd.in>
2025-07-04 19:47:19 +0530

    Day 96

---
+++ week_14/Day_96/kthCharacter.js
@@ -0,0 +1,33 @@
+function kthCharacter(k, operations) {
+    let shift = 0;
+    let lengths = [];
+    let len = 1;
+
+    for (let op of operations) {
+        len *= 2;
+        lengths.push(len);
+        if (len >= k) break;
+    }
+
+    for (let i = lengths.length - 1; i >= 0; i--) {
+        let half = lengths[i] / 2;
+        if (k > half) {
+            k -= half;
+            if (operations[i] === 1) shift++;
+        }
+    }
+
+    const baseCharCode = 'a'.charCodeAt(0);
+    return String.fromCharCode((shift % 26) + baseCharCode);
+}
+
+
+function main() {
+    const k = 6;
+    const operations = [1, 0, 1, 1]; 
+
+    const result = kthCharacter(k, operations);
+    console.log(`The ${k}th character is: ${result}`);
+}
+
+main();
